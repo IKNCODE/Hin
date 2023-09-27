1ea676cd7e9f45e01c423a6bcbc6d2aa914c2112
IKNCODE <ajkaen863@gmail.com>
2023-09-27 06:15:28 +0300

    add main test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,11 @@
-from fastapi.testclient import TestClient
-
+import pytest
+from httpx import AsyncClient
 from src.main import app
 
-client = TestClient(app)
+@pytest.mark.anyio
+async def test_main():
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+            response = await ac.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"request" : "", "response" : ""}
 
